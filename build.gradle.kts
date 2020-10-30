0933470b979fb4cacde8b59089e966d3cf3ef59c
Richard Borge <richard.borge@nav.no>
2020-10-30 14:45:32 +0100

    Initial commit, token check and ldap

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val springSleuthVersion = "2.2.3.RELEASE"
+val tokenValidationVersion = "1.3.0"
+val oidcSupportVersion = "0.2.18"
+val logstashVersion = "5.1"
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
+    id("org.springframework.boot") version "2.3.4.RELEASE"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.4.10"
+    idea
+}
+
+repositories {
+    mavenCentral()
+    maven ("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.cloud:spring-cloud-starter-sleuth:$springSleuthVersion")
+
+    implementation("no.nav.security:token-validation-spring:$tokenValidationVersion")
+    implementation("no.nav.security:token-client-spring:$tokenValidationVersion")
+    implementation("no.nav.security:oidc-spring-support:$oidcSupportVersion")
+
+    implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("ch.qos.logback:logback-classic")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
+}
+
+idea {
+    module {
+        isDownloadJavadoc = true
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
---
+++ src/main/kotlin/no/nav/klage/Application.kt
@@ -0,0 +1,11 @@
+package no.nav.klage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main() {
+    runApplication<Application>()
+}
