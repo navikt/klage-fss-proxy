04ae9b2172377db86c667bdeef1aa622cf0be330
Richard Borge <richard.borge@nav.no>
2020-11-02 13:16:49 +0100

    Set claim name for ident

--- src/main/kotlin/no/nav/klage/service/TokenService.kt
+++ src/main/kotlin/no/nav/klage/service/TokenService.kt
@@ -10,7 +10,7 @@ class TokenService(
 ) {
 
     fun getIdent(): String = tokenValidationContextHolder.tokenValidationContext
-            .getJwtToken(SecurityConfiguration.ISSUER_AAD).jwtTokenClaims?.get("todo")?.toString()
+            .getJwtToken(SecurityConfiguration.ISSUER_AAD).jwtTokenClaims?.get("NAVIdent")?.toString()
             ?: throw RuntimeException("Ident not found in token")
 
 }
