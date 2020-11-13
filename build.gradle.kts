57eb874bf1c0e3ab7dd1d10b661a11a857c0c8bc
Hågen Hasle <haagenhasle@gmail.com>
2020-11-13 21:37:30 +0100

    Lagt til dep til commons-pool

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("commons-pool:commons-pool")
     implementation("org.springframework.cloud:spring-cloud-starter-sleuth:$springSleuthVersion")
     implementation("org.springframework.ldap:spring-ldap-core:$springLdapVersion")
 
