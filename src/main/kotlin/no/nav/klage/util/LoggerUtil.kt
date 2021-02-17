fdf86b8f44525cfa44b21fbc283156f53e0071f3
Andreas Jonsson <andreas.jonsson@nav.no>
2021-02-17 11:39:54 +0100

    Remove audit logging from here. Is not in use.

--- src/main/kotlin/no/nav/klage/util/LoggerUtil.kt
+++ src/main/kotlin/no/nav/klage/util/LoggerUtil.kt
@@ -6,5 +6,3 @@ import org.slf4j.LoggerFactory
 fun getLogger(forClass: Class<*>): Logger = LoggerFactory.getLogger(forClass)
 
 fun getSecureLogger(): Logger = LoggerFactory.getLogger("secure")
-
-fun getAuditLogger(): Logger = LoggerFactory.getLogger("audit")
