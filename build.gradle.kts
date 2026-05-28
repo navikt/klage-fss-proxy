900f6e1e5f4f4eefadcb22ad85141d9b16990ad1
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-05-28 08:39:22 +0200

    Fix vulnerabilities, while waiting for fix in Spring Boot.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("commons-pool:commons-pool")
 
+    //Fix vulnerabilities, while waiting for fix in Spring Boot.
+    implementation("org.apache.tomcat.embed:tomcat-embed-core:11.0.22")
+    implementation("io.netty:netty-codec-http:4.2.14.Final")
+    implementation("io.netty:netty-codec-http2:4.2.14.Final")
+    implementation("io.netty:netty-codec-http3:4.2.14.Final")
+    implementation("io.netty:netty-codec-dns:4.2.14.Final")
+    implementation("io.netty:netty-codec-compression:4.2.14.Final")
+    implementation("io.netty:netty-transport-native-epoll:4.2.14.Final")
+    implementation("io.netty:netty-resolver-dns:4.2.14.Final")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation("no.nav.security:token-validation-spring:$tokenValidationVersion")
